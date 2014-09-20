07c7c69eec33285332f5bd3637118de0044b813e
pollo <pollo1_91@yahoo.it>
2014-09-20 18:23:43 +0200

    multiple heuristics

--- graph_partitioning.cpp
+++ graph_partitioning.cpp
@@ -10,15 +10,55 @@
 
 #define MAX_PARTITIONS 10000
 
+void run_heuristic(const char* heuristic_name,
+                   int partitions_number,
+                   const char* dataset,
+                   const Graph& graph)
+{
+  Partition partition;
+
+  time_t start = time(0);
+  if (strncmp(heuristic_name,"LDG",3) == 0)
+  {
+    linear_deterministic_greedy(graph,
+				partitions_number,
+                                &partition);
+  }
+  else if (strncmp(heuristic_name,"FENNEL",6) == 0)
+  {
+    fennel(graph,
+           partitions_number,
+           &partition);
+  }
+  else
+  {
+    printf("Invalid heuristic name %s\n",heuristic_name);
+    exit(1);
+  }
+
+  time_t end = time(0);
+  int time = difftime(end, start);
+  printf("Partitioned in %d seconds\n",time);
+
+  double fraction_edges_cut = graph.get_fraction_edges_cut(partition);
+  double maximum_load = graph.get_normalized_maximum_load(partition);
+  printf("Dataset %s with heuristic %s and %d partitions:\n",
+         dataset,
+         heuristic_name,
+         partitions_number);
+  printf("Fraction edges cut = %f\nNormalized maximum load = %f\n\n",
+         fraction_edges_cut,
+         maximum_load);
+}
+
 int main(int argc, char** argv)
 {
   Graph graph;
-  Partition partition;
 
   if (argc != 4)
   {
     printf("Invalid number of parameters\n");
-    printf("Usage: %s <dataset name> <heuristic name> <partitions number>\n",
+    printf("Usage: %s <dataset name> <heuristic names> <partitions number>\n",
            argv[0]);
     exit(1);
   }
@@ -38,8 +78,6 @@ int main(int argc, char** argv)
     exit(1);
   }
 
-  time_t start = time(0);
-
   int partitions_number = strtol(argv[3], NULL, 10);
   if (partitions_number < 1 || partitions_number > MAX_PARTITIONS)
   {
@@ -47,37 +85,24 @@ int main(int argc, char** argv)
     exit(1);
   }
 
-  if (strncmp(argv[2],"LDG",3) == 0)
+  bool heuristics_valid = false;
+  if (strstr(argv[2],"LDG") != NULL)
   {
-    linear_deterministic_greedy(graph,
-                                partitions_number,
-                                &partition);
+    run_heuristic("LDG",partitions_number,argv[1],graph);
+    heuristics_valid = true;
   }
-  else if (strncmp(argv[2],"FENNEL",6) == 0)
+
+  if (strstr(argv[2],"FENNEL") != NULL)
   {
-    fennel(graph,
-           partitions_number,
-           &partition);
+    run_heuristic("FENNEL",partitions_number,argv[1],graph);
+    heuristics_valid = true;
   }
-  else
+
+  if (!heuristics_valid)
   {
     printf("Invalid heuristic name %s\n",argv[2]);
     exit(1);
   }
-
-  time_t end = time(0);
-  int time = difftime(end, start);
-  printf("Partitioned in %d seconds\n",time);
-
-  double fraction_edges_cut = graph.get_fraction_edges_cut(partition);
-  double maximum_load = graph.get_normalized_maximum_load(partition);
-  printf("Dataset %s with heuristic %s and %d partitions:\n",
-         argv[1],
-         argv[2],
-         partitions_number);
-  printf("Fraction edges cut = %f\nNormalized maximum load = %f\n",
-         fraction_edges_cut,
-         maximum_load);
 
   return 0;
 }
